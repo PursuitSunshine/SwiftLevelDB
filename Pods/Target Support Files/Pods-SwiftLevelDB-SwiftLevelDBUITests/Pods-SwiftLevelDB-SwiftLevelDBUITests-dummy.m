c89f899b44f5832c5568226fe12dbc764d97c19f
Cherish <cherish@micous.com>
2021-12-06 15:34:21 +0800

    [delete] init project

--- Pods/Target Support Files/Pods-SwiftLevelDB-SwiftLevelDBUITests/Pods-SwiftLevelDB-SwiftLevelDBUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftLevelDB_SwiftLevelDBUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftLevelDB_SwiftLevelDBUITests
-@end
--- SwiftLevelDB/LevelDB/Test.mm
+++
@@ -1,8 +0,0 @@
-//
-//  Test.m
-//  SwiftLevelDB
-//
-//  Created by Cherish on 2021/7/16.
-//
-
-#import <Foundation/Foundation.h>
